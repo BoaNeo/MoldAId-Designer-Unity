f1fa3f72ef781783f5eb9d2ff71a9ef60917480d
Niels Jørgensen <nj@boaneo.com>
2026-06-13 14:35:48 +0200

    + Custom pin diameters from 1 to 12mm

--- Assets/Features/GuidePin.cs
+++ Assets/Features/GuidePin.cs
@@ -11,27 +11,14 @@ namespace Features
 {
 	public class GuidePin : Guide
 	{
-		[ShowProperty(values = new [] {"3mm", "4mm", "5mm", "6mm"})]
-		[FeatureInput] public DataRef<int> pinType { get; } = new DataRef<int>( 0 );
+		[ShowProperty(unit = "mm", min = 1, max = 12)]
+		[FeatureInput] public DataRef<float> pinSize { get; } = new(3.0f);
 
 		[ShowProperty(unit = "mm", min = 5, max = 250)]
 		[FeatureInput] public DataRef<float> length { get; } = new(0f);
 
-		private struct Pin
-		{
-			public float diameter;
-		}
-
-		private static Pin[] _pins = new[]
-		{
-			new Pin { diameter = 3.0f },
-			new Pin { diameter = 4.0f },
-			new Pin { diameter = 5.0f },
-			new Pin { diameter = 6.0f },
-		};
-
 		public static IEnumerator<IYield> Build(bool changing,
-			int pinType,
+			float pinSize,
 			float length,
 			XForm position,
 			MeshBuilder mold,
@@ -46,8 +33,7 @@ namespace Features
 			float h = length>0 ? length+penetration : 2*(bounds.extents.z+penetration);
 			MeshBuilder mb = new MeshBuilder();
 		
-			Pin pin = _pins[pinType];
-			float radius = pin.diameter / 2.0f;
+			float radius = pinSize / 2.0f;
 			mb.GenerateCylinder(radius, h, Shape.ArcLenToSegments(radius,PreferencesFile.current.arcLength));
 
 			Matrix4x4 m = Matrix4x4.Translate( new Vector3(0, 0, -penetration ) );
